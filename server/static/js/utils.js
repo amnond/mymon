7b8e4e490d7396e4fd256b9503ebf3df044c5751
Amnon David <amnon.david@gmail.com>
2017-04-02 23:32:31 +0300

    regex background color working

--- server/static/js/utils.js
+++ server/static/js/utils.js
@@ -1,6 +1,9 @@
 
 function map2array(map, idxname)
 {
+    if (typeof map != "object") {
+        return null;
+    }
     var arr = [];
     for (key in map) {
         var obj = JSON.parse(JSON.stringify(map[key]));
@@ -12,6 +15,9 @@ function map2array(map, idxname)
 
 function array2map(arr, idxname)
 {
+    if ( Object.prototype.toString.call( arr ) !== '[object Array]' ) {
+        return null
+    }
     var map = {};
     for (var i=0; i<arr.length; i++) {
         var obj = JSON.parse(JSON.stringify(arr[i]));
